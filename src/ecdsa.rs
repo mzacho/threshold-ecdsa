766282a0dd138c810d46e3e60eb3ce4b515e7472
Benjamin <benjamin.zachariae@gmail.com>
2023-11-29 12:28:59 +0100

    push

--- src/ecdsa.rs
+++ src/ecdsa.rs
@@ -1,1 +1,30 @@
-// TODO
+// https://docs.rs/fiat-crypto/0.2.5/fiat_crypto/index.html
+// https://docs.rs/k256/latest/k256/
+
+// https://docs.rs/elliptic-curve/latest/elliptic_curve/
+use ecdsa::Signature;
+use elliptic_curve::{group::prime::PrimeCurveAffine, point::*};
+
+use crate::shares::Shares;
+
+pub fn run_ecdsa() {
+    todo!()
+}
+
+pub fn circuit_ecdsa() {
+    todo!()
+}
+
+/// Convert shares of a value "a" to a share of the representation of the point on the curve for the value "a"
+pub fn convert<Curve: PrimeCurveAffine>(
+    shares: Shares,
+    curve: Curve,
+) {
+  let g = Curve::generator().mul(shares.x);
+
+  let group_shares = Shares {
+    x: shares.x.mul(&g),
+    y: g,
+    m: shares.m,
+  };
+}
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod nat;
 mod node;
 mod schnorr;
 mod shares;
+mod ecdsa;
 
 const ECDSA: &str = "ecdsa";
 const SCHNORR: &str = "schnorr";
